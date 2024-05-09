1dd1b0557b1094ec27c12b975fd82c56a05d371e
Satyam Sidapara <satyamsidapara369@gmail.com>
2024-05-09 16:53:51 +0530

    added home page ui

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,11 +1,30 @@
 import Footer from 'Components/Footer/Footer'
 import React from 'react'
+import Logo from 'Assets/Images/logo-color.png'
 
 function Home() {
   return (
-    <div>
-      Hello Satyam    
-    <Footer />
+    <div className='flex flex-col items-center justify-center gap-20 h-[100vh]'>
+        
+        <div className=' h-48 w-48'>
+           <img className='w-full h-full' alt="logo" src={Logo} />      
+        </div>
+
+        <div className='flex justify-around items-center gap-13'>
+            <div className='w-1/2 text-center font-semibold basis-1/2'>
+                <h1 className='text-white text-5xl tracking-widest leading-tight'>
+                    BookShelf <br />
+                    <span className=' text-warning'>Your personal library and social network for bookworms</span>
+                </h1>
+            </div>
+
+            <div className=' mr-20'>
+              <button className='btn btn-primary rounded-md px-3 py-1 text-2xl px-5 py-2'>Register</button>
+              <button className='btn btn-warning mx-3 rounded-md px-3 py-1 text-2xl px-5 py-2'>Login</button>
+            </div>
+
+        </div>
+
     </div>
   )
 }
